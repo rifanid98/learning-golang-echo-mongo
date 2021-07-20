67586b52688ce2c8374261f2978a23fc4069d5bd
rifanid98 <adninsijawa.office@gmail.com>
2021-07-21 00:50:27 +0700

    connect to mongodb

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learning-golang-mongo
 
 go 1.16
+
+require go.mongodb.org/mongo-driver v1.6.0
--- main.go
+++ main.go
@@ -1,5 +1,72 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Product describes an electronic product e.g. phone
+type Product struct {
+	ID          primitive.ObjectID `json:"_id" bson:"_id"`
+	Name        string             `json:"name" bson:"name"`
+	Price       int                `json:"price" bson:"price"`
+	Currency    string             `json:"currency" bson:"currency"`
+	Quantity    string             `json:"quantity" bson:"quantity"`
+	Discount    int                `json:"discount,omitempty" bson:"discount,omitempty"`
+	Vendor      string             `json:"vendor" bson:"vendor"`
+	Accessories []string           `json:"accessories,omitempty" bson:"accessories,omitempty"`
+	SkuID       string             `json:"sku_id" bson:"sku_id"`
+}
+
+var iphone10 = &Product{
+	ID:          primitive.NewObjectID(),
+	Name:        "iPhone 10",
+	Price:       900,
+	Currency:    "USD",
+	Quantity:    "40",
+	Vendor:      "Apple",
+	Accessories: []string{"charger", "earphone", "slotopener"},
+	SkuID:       "1234",
+}
+
+var trimmer = &Product{
+	ID:          primitive.NewObjectID(),
+	Name:        "Easy Trimmer",
+	Price:       120,
+	Currency:    "USD",
+	Quantity:    "300",
+	Vendor:      "Philips",
+	Discount:    7,
+	Accessories: []string{"charger", "comb", "bladeset", "cleaning oil"},
+	SkuID:       "2345",
+}
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	db := client.Database("tronics")
+	collection := db.Collection("products")
+
+	res, err := collection.InsertOne(context.Background(), iphone10)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(res.InsertedID.(primitive.ObjectID).Timestamp())
 }
